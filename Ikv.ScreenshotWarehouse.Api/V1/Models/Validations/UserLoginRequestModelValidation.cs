64fb59844990f84a6a39e242899b986dfd439305
kubilayeldemir <kubi0707@gmail.com>
2022-02-02 23:22:38 +0300

    Static validations added for user login and register

---
+++ Ikv.ScreenshotWarehouse.Api/V1/Models/Validations/UserLoginRequestModelValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Ikv.ScreenshotWarehouse.Api.V1.Models.RequestModels;
+
+namespace Ikv.ScreenshotWarehouse.Api.V1.Models.Validations
+{
+    public class UserLoginRequestModelValidation : AbstractValidator<UserLoginRequestModel>
+    {
+        public UserLoginRequestModelValidation()
+        {
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı adı alanı boş olamaz.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş olamaz.");
+        }
+    }
+}
---
+++ Ikv.ScreenshotWarehouse.Api/V1/Models/Validations/UserRegisterRequestModelValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Ikv.ScreenshotWarehouse.Api.V1.Models.RequestModels;
+
+namespace Ikv.ScreenshotWarehouse.Api.V1.Models.Validations
+{
+    public class UserRegisterRequestModelValidation : AbstractValidator<UserRegisterRequestModel>
+    {
+        public UserRegisterRequestModelValidation()
+        {
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı boş olamaz.");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı adı alanı boş olamaz.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş olamaz.");
+        }
+    }
+}
