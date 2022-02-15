a044111eae226956f0d81b2cdbce9be4de4ef404
kubilayeldemir <kubi0707@gmail.com>
2022-02-15 23:06:57 +0300

    feat: Save postFile as base64 in a table(PostRawData) impl.

---
+++ Ikv.ScreenshotWarehouse.Api/Persistence/Entities/PostRawData.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ikv.ScreenshotWarehouse.Api.Persistence.Entities
+{
+    public class PostRawData : DateField
+    {
+        [Key]
+        public long Id { get; set; }
+        public string FileBase64 { get; set; }
+        public string FileMd5 { get; set; }
+        public string PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
