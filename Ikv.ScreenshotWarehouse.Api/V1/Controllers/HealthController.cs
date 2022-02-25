6bb1f8f4840c56b410d687705c8e2dce0f787558
kubilayeldemir <kubi0707@gmail.com>
2022-02-25 22:24:36 +0300

    Health check EP added

---
+++ Ikv.ScreenshotWarehouse.Api/V1/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ikv.ScreenshotWarehouse.Api.V1.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> HealthCheck()
+        {
+            return Ok();
+        }
+    }
+}
