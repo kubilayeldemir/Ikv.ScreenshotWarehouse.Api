f211c4f31078b8248587285ea3e5a41b39a2661e
kubilayeldemir <kubi0707@gmail.com>
2024-01-21 16:43:17 +0300

    Push missing migration files to remote

---
+++ Ikv.ScreenshotWarehouse.Api/Migrations/20220505091130_IdUseVarchar.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ikv.ScreenshotWarehouse.Api.Migrations
+{
+    public partial class IdUseVarchar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Posts",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostId",
+                table: "PostRawData",
+                type: "character varying(64)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Posts",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostId",
+                table: "PostRawData",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(64)",
+                oldNullable: true);
+        }
+    }
+}
